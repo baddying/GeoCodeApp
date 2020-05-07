cb9b856b8bef6c1c3f3550dbfae1514a978856aa
baddying <38316011+baddying@users.noreply.github.com>
2020-05-07 09:33:01 +0800

    Add files via upload

---
+++ MainForm.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 31);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(385, 639);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(597, 31);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(371, 613);
+            this.richTextBox2.TabIndex = 1;
+            this.richTextBox2.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(412, 217);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(167, 45);
+            this.button1.TabIndex = 2;
+            this.button1.Text = ">>>>";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(412, 279);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(167, 45);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "<<<<";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 19);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "地址：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(630, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(161, 19);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "坐标（经纬度）：";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1018, 682);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "MainForm";
+            this.Text = "MainForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Newtonsoft.Json;
+
+namespace WindowsFormsApp1
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string[] dzs = richTextBox1.Text.Split();
+            foreach (var dz in dzs)
+            {
+                // http://api.tianditu.gov.cn/geocoder?ds={"keyWord":"延庆区北京市延庆区延庆镇莲花池村前街50夕阳红养老院"}&tk=您的密钥             
+
+                //  string m_CurrentSearchUri = string.Format(@"http://api.tianditu.gov.cn/geocoder?ds={""keyWord"":{0}}&tk={1}",dz,"d8b05b26b44eb9007c645c51d20a0128") ;
+
+                string a = @"http://api.tianditu.gov.cn/geocoder?ds=";
+                string b = @"{""keyWord"":""xxxx""}";
+                b= b.Replace("xxxx", dz);
+                string c = @"&tk=d8b05b26b44eb9007c645c51d20a0128";
+                string m_CurrentSearchUri = string.Format("{0}{1}{2}",a,b,c);
+
+             
+                // 发送要求
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(m_CurrentSearchUri);
+                //下面的代码一定要加上，在天地图网站会发生"远程服务器返回错误: (403) 已禁止。"
+                request.Method = "GET";
+                request.Accept = "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8";
+                request.Headers.Add("Accept-Language", "zh-cn,zh;q=0.8,en-us;q=0.5,en;q=0.3");
+                request.UserAgent = "Mozilla/5.0 (Windows NT 5.2; rv:12.0) Gecko/20100101 Firefox/12.0";
+
+                // 取得响应
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                StreamReader sr = new StreamReader(response.GetResponseStream());
+                //得到一个json字符串
+                string joResultTemp = sr.ReadToEnd().ToString();
+                //将返回的json数据转为JSON对象
+                RecordResultTDT joResult = JsonConvert.DeserializeObject<RecordResultTDT>(joResultTemp);
+                //返回天地图的Json对象
+                string lonlat = "\n";
+                
+                if (joResult.Msg != null&&joResult.Msg.ToString().ToUpper() == "OK")
+                {
+                    lonlat = joResult.Location.Lon.ToString() + "," + joResult.Location.Lat.ToString() + "\n";
+                }
+
+                richTextBox2.Text += lonlat;
+            }
+           
+      
+        }
+    }
+
+    public class LocationTDT
+    {
+        [JsonProperty("level")]
+        public string Level { get; set; }
+
+        [JsonProperty("lon")]
+        public double Lon { get; set; }
+
+        [JsonProperty("lat")]
+        public double Lat { get; set; }
+
+        [JsonProperty("keyWord")]
+        public string KeyWord { get; set; }
+    }
+    class RecordResultTDT
+    {
+        [JsonProperty("msg")]
+        public string Msg { get; set; }
+
+        [JsonProperty("location")]
+        public LocationTDT Location { get; set; }
+
+        [JsonProperty("searchVersion")]
+        public string SearchVersion { get; set; }
+
+        [JsonProperty("status")]
+        public int Status { get; set; }
+    }
+}
